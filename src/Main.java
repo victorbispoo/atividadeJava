1e6962ffb25b5c3e8d462cc2a347c3de46db2d09
victorbispoo <victorbispo2008@gmail.com>
2026-01-29 11:13:35 -0300

    Começo do CRUD

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    static ArrayList<String> listaAlunos = new ArrayList<>();
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String aluno1= "victor";
+        String aluno2= "kaique";
+        String aluno3= "davi";
+        listaAlunos.add(aluno1);
+        listaAlunos.add(aluno2);
+        listaAlunos.add(aluno3);
+        System.out.println("Bem-vindo ao Gerenciador de Alunos\nSelecione o que quer fazer:\n[1]Listar Aluno\n[2]Excluir Aluno\n[3]Editar Aluno\n[4]Adicionar Aluno");
+
+        Scanner sc = new Scanner(System.in);
+        String escolha = sc.nextLine();
+
+        switch (escolha){
+            case "1":
+                escolha= "1";
+            listarAlunos();
+            break;
+            case "2":
+                escolha="2";
+            listarAlunos();
+            System.out.println("Selecione qual aluno excluir");
+            Scanner scan = new Scanner(System.in);
+            String aluno = scan.nextLine();
+            listaAlunos.remove(aluno);
         }
     }
-}
+    public static void listarAlunos(){
+        System.out.println(listaAlunos);
+    }
+    }
